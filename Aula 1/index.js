c67c103be865db9aa405ac1b9b68f339731fdf92
longhiisa <https://github.com/longhiisa/Back-end--Isabela.git>
2025-02-11 10:54:27 -0300

    aula1

---
+++ Aula 1/index.js
@@ -0,0 +1,30 @@
+const express = require(`express`);
+
+const app = express();
+
+const port = 3000;
+
+app.get(`/`, (req, res) =>{
+    res.send(`Pagina principal`);
+});
+
+app.get(`/home`, (req, res) =>{
+    res.send(`Pagina home`);
+});
+
+app.get(`/login`, (req, res) =>{
+    res.send(`Pagina login`);
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () =>{
+    console.log(`Servidor eodando na porta`, port);
+});
